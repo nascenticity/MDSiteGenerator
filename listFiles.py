77966455b907e98d181b8dbeb4a45582c1873acf
Raye <rayemayberry@yahoo.com>
2025-03-12 16:09:03 -0500

    include only md files & folders containing md files

--- listFiles.py
+++ listFiles.py
@@ -1,18 +1,20 @@
 import os
 
 #set target directory
-target_dir = "./testDir"
+target_dir = input("Enter the path to the directory you want to create a tree from: ")
 
 #list everything inside target_dir
 
 dir_tree = {}
 
 for dirpath, dirnames, files in os.walk(target_dir):
-    print(f'Found directory: {dirpath}')
-    dir_tree.update({dirpath : files})
+    # print(f'Found directory: {dirpath}')
+    temp_index = []
     for file_name in files:
-        print(file_name)
-        
+        if file_name.endswith(".md"):
+            temp_index.append(file_name)
+    if len(temp_index) >0:
+        dir_tree.update({dirpath : temp_index})
 
 print("Directory Tree:")
 print(dir_tree)
