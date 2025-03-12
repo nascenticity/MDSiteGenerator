92704bac3acd21c421cd5f4a05f0862a56ca9ac6
Raye <rayemayberry@yahoo.com>
2025-03-12 14:38:12 -0500

    basic script generates successful rudimentary html

--- sitebuilder.py
+++ sitebuilder.py
@@ -1,5 +1,5 @@
 # import tinyhtml for creating html elements, and markdown for converting markdown to html strings
-from tinyhtml import *
+# from tinyhtml import *
 from markdown import *
 
 #get file contents & convert to html
@@ -11,10 +11,19 @@ with open ("testMD.md", "r") as f:
 print (output)
 
 # use tinyhtml to create an empty html template
-html_content = html(lang="en")(
-    h("head")(),
-    h("body")(
-       output
-    ),
-)
-print(html_content.render()) 
+# html_content = html(lang="en")(
+  #  h("head")(
+   #     h("meta", charset="utf-8")
+    #),
+    # h("body")(
+      # frag(output)
+    # ),
+#)
+#print(html_content.render()) 
+
+pageTitle = "Hello World"
+
+html_template = f'<!DOCTYPE html><html lang="en"><head><meta charset="utf-8"><title>{pageTitle}</title> <link rel="stylesheet" href="style.css"> </head><body>{output}</body></html>'
+
+with open("testMD.html", "w") as f2:
+    f2.write(html_template)
