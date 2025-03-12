41f851cd0a461b1d74b9e24cae0939079ea106b9
Raye <rayemayberry@yahoo.com>
2025-03-12 15:39:10 -0500

    generate dictionary of directories & files

--- listFiles.py
+++ listFiles.py
@@ -4,7 +4,15 @@ import os
 target_dir = "./testDir"
 
 #list everything inside target_dir
+
+dir_tree = {}
+
 for dirpath, dirnames, files in os.walk(target_dir):
     print(f'Found directory: {dirpath}')
+    dir_tree.update({dirpath : files})
     for file_name in files:
         print(file_name)
+        
+
+print("Directory Tree:")
+print(dir_tree)
