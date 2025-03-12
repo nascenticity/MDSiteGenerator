3afe6a51038529e467875c262dea72e4f3bc34d7
Raye <rayemayberry@yahoo.com>
2025-03-12 18:29:48 -0500

    create index.html & style.css

--- MDSiteBuilder.py
+++ MDSiteBuilder.py
@@ -1,11 +1,13 @@
 from create_dir_tree import create_dir_tree
 from make_HTML import make_html
 
-# create a list of files to convert
+
 target = input("Enter the path to the directory: ")
 
+# create a list of files to convert
 tree = create_dir_tree(target)
 
+#convert each file
 for subdir in tree:
     #print(subdir)
     for entry in tree[subdir]:
@@ -14,3 +16,10 @@ for subdir in tree:
             #needs full filepath
             make_html(f"{subdir}/{entry}")
             print(f"Converted {subdir}/{entry} to HTML")
+
+#generate index.html and style.css
+with open(f"{target}/index.html", "w") as f2:
+    f2.write("")
+
+with open(f"{target}/style.css", "w") as f3:
+    f3.write("")
---
+++ testDir/subDir/testMD2.md
@@ -0,0 +1,1 @@
+[link to another file](../testMD.md)
