a0c1f62f9e3d12e71e0dfc3619d32d3b2fa7b14e
Raye <rayemayberry@yahoo.com>
2025-03-12 21:47:37 -0500

    do not generate README if one doesnt exist

--- make_index.py
+++ make_index.py
@@ -8,30 +8,28 @@ def make_index(source_dir, destination):
     try:
         os.scandir(f"{source_dir}/README.md")
     except:
-        with open(f"{source_dir}/README.md", "w") as r:
-            r.write("")
- 
+        temp_html = ""
+    else: 
+        #convert README.md file to temp html file
+        markdownFromFile(
+            input= f"{source_dir}/README.md",
+            output=f'{destination}/index.html',
+            encoding='utf8',
+        )
     
-    #convert README.md file to temp html file
-    markdownFromFile(
-        input= f"{source_dir}/README.md",
-        output=f'{destination}/index.html',
-        encoding='utf8',
-    )
-    
-    #convert links referencing .md files & replace references with the equivalent .html files
-    with open(f"{destination}/index.html", "r+") as f:
-        contents = f.read()
-        contents = contents.replace(".md",".html")
-        #resetting to the 0th character in the file is necessary to force contents to be overwritten
-        f.seek(0)
-        f.write(contents)
+        #convert links referencing .md files & replace references with the equivalent .html files
+        with open(f"{destination}/index.html", "r+") as f:
+            contents = f.read()
+            contents = contents.replace(".md",".html")
+            #resetting to the 0th character in the file is necessary to force contents to be overwritten
+            f.seek(0)
+            f.write(contents)
 
-    #read temp html back in using Beautiful Soup
-    with open(f"{destination}/index.html", "r") as f2:
-        temp_html = BeautifulSoup(f2.read())
+        #read temp html back in using Beautiful Soup
+        with open(f"{destination}/index.html", "r") as f2:
+            temp_html = BeautifulSoup(f2.read())
 
-    #print(temp_html)
+        #print(temp_html)
 
     #generate template as BeautifulSoup object
     with open("./template.html", "r") as t:
